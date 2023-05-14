b9826662f9a4b94465452b6a9a127f8dcc4017fd
Evan-aja <gebel909@student.ub.ac.id>
2023-05-14 08:06:19 +0700

    added test and moved functions to individual files

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
+	"kawan-usaha-api/server"
 	"log"
-	"net/http"
-	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -16,40 +14,7 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	// Gin Framework
-	gin.SetMode(os.Getenv("GIN_MODE"))
-	r := gin.Default()
-	r.SetTrustedProxies(
-		[]string{
-			os.Getenv("PROXY_1"),
-			os.Getenv("PROXY_2"),
-			os.Getenv("PROXY_3"),
-		},
-	)
-
-	//CORS
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		if c.Request.Method == "OPTIONS" {
-			c.Writer.Header().Set("Content-Type", "application/json")
-			c.AbortWithStatus(204)
-		} else {
-			c.Next()
-		}
-	})
-
-	//Routers
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to Kawan Usaha API!",
-			"success": true,
-		})
-	})
+	r := server.SetupRouter()
 
 	//Server init
 
