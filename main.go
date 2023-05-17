0f448b99f92942700a67a690b5a6d7e35ece278f
Evan-aja <gebel909@student.ub.ac.id>
2023-05-17 19:34:42 +0700

    email for single and multiple receiver complete

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"kawan-usaha-api/server"
 	"kawan-usaha-api/server/lib"
 	"log"
-	"time"
 )
 
 func main() {
@@ -15,22 +14,6 @@ func main() {
 	lib.EnvLoader()
 
 	r := server.SetupRouter()
-
-	var result string
-	var err error
-
-	if result, err = lib.GenerateToken(time.Duration(30), "123123123 ini username", "t"); err != nil {
-		log.Fatal(err.Error())
-		return
-	}
-	fmt.Println(result)
-
-	var results interface{}
-	if results, err = lib.ValidateToken(result, "t"); err != nil {
-		log.Fatal(err.Error())
-		return
-	}
-	fmt.Println(results)
 
 	//Server init
 	if err := r.Run(":5000"); err != nil {
--- server/lib/email.go
+++ server/lib/email.go
@@ -1,39 +1,103 @@
 package lib
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"net/smtp"
 )
 
-func SendMailMultipleReceiver(receiver []string, message []byte) {
-	auth := smtp.PlainAuth(os.Getenv("EMAIL_FROM"), os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASS"), os.Getenv("EMAIL_HOST"))
-
-	to := strings.Join(receiver, ",")
-
-	msg := []byte("To: " + to + "\r\n" +
-		"\r\n" +
-		"Good afternoon!!!\r\n")
-	err := smtp.SendMail(os.Getenv("EMAIL_HOST")+":"+os.Getenv("EMAIL_PORT"), auth, os.Getenv("EMAIL_USER"), receiver, msg)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
+type EmailData struct {
+	Code      string
+	FirstName string
+	Subject   string
 }
 
-func SendMailSingleReceiver(receiver string, message []byte) {
+var Templates = map[string]string{
+	"verify":    "verificationCode.html",
+	"promotion": "promotion.txt",
+}
+
+func SendMailSingleReceiver(receiver string, data *EmailData, templateName string) {
+	go func() {
+		auth := smtp.PlainAuth(os.Getenv("EMAIL_FROM"), os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASS"), os.Getenv("EMAIL_HOST"))
+
+		var body bytes.Buffer
+		template, err := ParseTemplateDir("templates")
+		if err != nil {
+			log.Fatal("Could not parse template", err)
+		}
+
+		template.ExecuteTemplate(&body, templateName, &data)
+
+		msg := []byte(
+			fmt.Sprintf(
+				"From: %s\r\n"+
+					"To: %s\r\n"+
+					"Content-Type: text/html; charset=UTF-8\r\n"+
+					"Subject: %s\r\n"+
+					"\r\n"+
+					"%s", os.Getenv("EMAIL_FROM"), receiver, data.Subject, body.String(),
+			),
+		)
+
+		errsend := smtp.SendMail(os.Getenv("EMAIL_HOST")+":"+os.Getenv("EMAIL_PORT"), auth, os.Getenv("EMAIL_USER"), []string{receiver}, msg)
+
+		if errsend != nil {
+			log.Fatal(err)
+		}
+	}()
+}
+
+func SendMailMultipleReceiver(receivers []string, data *EmailData, templateName string) {
 	auth := smtp.PlainAuth(os.Getenv("EMAIL_FROM"), os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASS"), os.Getenv("EMAIL_HOST"))
 
-	msg := []byte("To: " + receiver + "\r\n" +
-		"\r\n" +
-		"Good afternoon!!!\r\n")
-	err := smtp.SendMail(os.Getenv("EMAIL_HOST")+":"+os.Getenv("EMAIL_PORT"), auth, os.Getenv("EMAIL_USER"), []string{receiver}, msg)
-
+	var body bytes.Buffer
+	template, err := ParseTemplateDir("templates")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Could not parse template", err)
 	}
 
+	template.ExecuteTemplate(&body, templateName, &data)
+
+	msg := []byte(
+		fmt.Sprintf(
+			"From: %s\r\n"+
+				"To: %s\r\n"+
+				"Content-Type: text/html; charset=UTF-8\r\n"+
+				"Subject: %s\r\n"+
+				"\r\n"+
+				"%s", os.Getenv("EMAIL_FROM"), strings.Join(receivers, ","), data.Subject, body.String(),
+		),
+	)
+
+	errsend := smtp.SendMail(os.Getenv("EMAIL_HOST")+":"+os.Getenv("EMAIL_PORT"), auth, os.Getenv("EMAIL_USER"), receivers, msg)
+
+	if errsend != nil {
+		log.Fatal(err)
+	}
+}
+
+func ParseTemplateDir(dir string) (*template.Template, error) {
+	var paths []string
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			paths = append(paths, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return template.ParseFiles(paths...)
 }
